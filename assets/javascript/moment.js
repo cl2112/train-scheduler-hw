44d9b3fda5f5be0c0b2c86b3296e9b537a55f7e0
cl2112 <cl2112cl2112@gmail.com>
2017-03-03 11:16:39 -0500

    finished the form and text inputs for the add train section in index.html

---
+++ assets/javascript/moment.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA2-RGakh-PPWkcsDND744s8g7H5YWQMfM",
+  authDomain: "super-train-station.firebaseapp.com",
+  databaseURL: "https://super-train-station.firebaseio.com",
+  storageBucket: "super-train-station.appspot.com",
+  messagingSenderId: "243788238099"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+console.log(database);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
